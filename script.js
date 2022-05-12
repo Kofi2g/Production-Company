cfb614f043ea31d73ee0b9048e4e4fc516210c31
Kofi <sambonful99@hotmail.com>
2022-05-12 18:26:24 +0100

    Completed sidebar with styles, added JS as well as HTML adjustments

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-const close = document.querySelector("#close")
-const open = document.querySelector("#open")
+const close = document.querySelector("#close");
+const open = document.querySelector("#open");
+const a = document.querySelectorAll("a");
 
 
 close.addEventListener("click", () => {
-    sidebar.style.visibility = "hidden"
-})
+    sidebar.style.visibility = "hidden";
+
+    for (let link of a) {
+        link.style.display = "none"
+    }
+});
 
 open.addEventListener("click", () => {
-    sidebar.style.visibility = "visible"
-})
+    sidebar.style.visibility = "visible";
+
+    for (let link of a) {
+        link.style.display = ""
+
+    }
+});
