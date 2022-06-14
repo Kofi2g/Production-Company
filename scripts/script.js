b7810eb927f79f625a50bb1b48864926441d788c
Kofi <sambonful99@hotmail.com>
2022-06-14 23:57:48 +0100

    Significant overhaul to Navigation

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,20 @@
-const close = document.querySelector("#close");
-const open = document.querySelector("#open");
-const a = document.querySelectorAll("a");
+// const close = document.querySelector("#close");
+// const open = document.querySelector("#open");
+// const a = document.querySelectorAll("a");
 
-close.addEventListener("click", () => {
-    sidebar.style.visibility = "hidden";
+// close.addEventListener("click", () => {
+//     sidebar.style.visibility = "hidden";
 
-    for (let link of a) {
-        link.style.display = "none"
-    }
-});
+//     for (let link of a) {
+//         link.style.display = "none"
+//     }
+// });
 
-open.addEventListener("click", () => {
-    sidebar.style.visibility = "visible";
+// open.addEventListener("click", () => {
+//     sidebar.style.visibility = "visible";
 
-    for (let link of a) {
-        link.style.display = ""
+//     for (let link of a) {
+//         link.style.display = ""
 
-    }
-});
+//     }
+// });
